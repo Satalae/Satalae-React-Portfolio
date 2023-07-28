5d4f881cdc24a4c15b01ae2704814c7ba0092f19
Satalae <harrison19988@gmail.com>
2023-07-27 20:56:06 -0700

    All images added, css for Portfolio req

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -40,20 +40,61 @@ function Portfolio() {
             <h2>Portfolio</h2>
             
                 <div className='image-text'>
-                    <img src='./images/frank.jpg' alt='projectFrank'/>
+                    <img src='./images/Chal1.png' alt='project1'/>
                     <div className='challenge-text'>Challenge 1</div>
                 </div>
                 <div className='image-text'>
-                    <img src='./images/frank.jpg' alt='projectFrank'/>
+                    <img src='./images/Chal2.png' alt='project2'/>
+                    <div className='challenge-text'>Challenge 2</div>
                 </div>
-                <img src='./images/frank.jpg' alt='projectFrank'/>
-                <img src='./images/frank.jpg' alt='projectFrank'/>
-                <img src='./images/frank.jpg' alt='projectFrank'/>
-                <img src='./images/frank.jpg' alt='projectFrank'/>
-                <img src='./images/frank.jpg' alt='projectFrank'/>
-                <img src='./images/frank.jpg' alt='projectFrank'/>
-                <img src='./images/frank.jpg' alt='projectFrank'/>
-                <img src='./images/frank.jpg' alt='projectFrank'/>
+                <div className='image-text'>
+                    <img src='./images/Chal3.png' alt='project3'/>
+                    <div className='challenge-text'>Challenge 3</div>
+                </div>
+                <div className='image-text'>
+                    <img src='./images/Chal4.png' alt='project4'/>
+                    <div className='challenge-text'>Challenge 4</div>
+                </div>
+                <div className='image-text'>
+                    <img src='./images/Chal5.png' alt='project5'/>
+                    <div className='challenge-text'>Challenge 5</div>
+                </div>
+                <div className='image-text'>
+                    <img src='./images/Chal6.png' alt='project6'/>
+                    <div className='challenge-text'>Challenge 6</div>
+                </div>
+                <div className='image-text'>
+                    <img src='./images/Chal9.png' alt='project9'/>
+                    <div className='challenge-text'>Challenge 9</div>
+                </div>
+                <div className='image-text'>
+                    <img src='./images/Chal10.png' alt='project10'/>
+                    <div className='challenge-text'>Challenge 10</div>
+                </div>
+                <div className='image-text'>
+                    <img src='./images/Chal11.png' alt='project11'/>
+                    <div className='challenge-text'>Challenge 11</div>
+                </div>
+                <div className='image-text'>
+                    <img src='./images/Chal12.png' alt='project12'/>
+                    <div className='challenge-text'>Challenge 12</div>
+                </div>
+                <div className='image-text'>
+                    <img src='./images/Chal13.png' alt='project13'/>
+                    <div className='challenge-text'>Challenge 13</div>
+                </div>
+                <div className='image-text'>
+                    <img src='./images/Frank.jpg' alt='project17'/>
+                    <div className='challenge-text'>Challenge 17</div>
+                </div>
+                <div className='image-text'>
+                    <img src='./images/Chal18.png' alt='project18'/>
+                    <div className='challenge-text'>Challenge 18</div>
+                </div>
+                <div className='image-text'>
+                    <img src='./images/Chal19.png' alt='project19'/>
+                    <div className='challenge-text'>Challenge 19</div>
+                </div>
             
         </div>
     );
